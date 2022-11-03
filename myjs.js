c38311056b15f0be24ff4e2342c1054aef6075f0
IgorGayvoronskiy <djdjdcs007@gmail.com>
2022-11-03 19:11:48 +0300

    1

---
+++ myjs.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
+});
+
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+    let b = document.getElementById("b1");
+    b.addEventListener("click", function(){
+        let stateObj = {
+            index: "bar",
+        }
+        history.pushState(null, 'page 2', 'bar.html');
+        window.addEventListener('popstate', function(){
+            PopUpShow();
+        })
+    });
+});
+
+// b.addEventListener('click', () => {
+//     window.history.pushState(
+//       {},
+//       'form',
+//       'form.html'
+//     )
+//     PopUpShow()
+//   })
+
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+}
