55ed6a255f496ebc3cd697629c8c843a8e775c5a
IgorGayvoronskiy <djdjdcs007@gmail.com>
2022-11-10 20:12:50 +0300

    8

--- myjs.js
+++ myjs.js
@@ -46,33 +46,8 @@ function PopUpHide(){
     $("#popup1").hide();
 }
 
-$( document ).ready(function() {
-    $.ajax({
-    url: 'https://api.slapform.com/your@email.com',
-     dataType: 'json',
-     method: 'POST',
-     data: {
-      name: 'Jon Snow',
-      message: 'Hello World! This is my first Slapform submission.',
-      /* Name triggers */
-      slap_subject: 'My Favorite Message',
-      slap_replyto: 'custom@replyto.com',
-      slap_debug: false,
-      slap_webhook: 'https://yourwebsite.com/webhook',
-      lap_honey: ''
-      /* These Slapform Name Triggers exist but aren't applicable to AJAX submissions */
-      // slap_redirect: 'https://yourwebsite.com',
-      // slap_captcha: false,
-     },
-     success: function (response) {
-      console.log('Got data: ', response);
-      if (response.meta.status == 'success') {
-       console.log('Submission was successful!');
-       // window.location.href = 'https://google.com' // Uncomment this line if you want to redirect the user upon a successful submission
-      } else if (response.meta.status == 'fail') {
-       console.log('Submission failed with these errors: ', response.meta.errors);
-      }
-    }
-   });
-});
-   
+saveButton.addEventListener('click', function () {
+    window.localStorage.setItem('value', area.value);
+    window.localStorage.setItem('timestamp', (new Date()).getTime());
+   }, false);
+   textarea.value = window.localStorage.getItem('value');
