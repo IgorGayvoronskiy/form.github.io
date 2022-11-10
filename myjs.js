30be98802bc5e943b84a117230738411781a9765
IgorGayvoronskiy <djdjdcs007@gmail.com>
2022-11-10 20:00:37 +0300

    7

--- myjs.js
+++ myjs.js
@@ -45,3 +45,34 @@ window.addEventListener('popstate', function(){
 function PopUpHide(){
     $("#popup1").hide();
 }
+
+$( document ).ready(function() {
+    $.ajax({
+    url: 'https://api.slapform.com/your@email.com',
+     dataType: 'json',
+     method: 'POST',
+     data: {
+      name: 'Jon Snow',
+      message: 'Hello World! This is my first Slapform submission.',
+      /* Name triggers */
+      slap_subject: 'My Favorite Message',
+      slap_replyto: 'custom@replyto.com',
+      slap_debug: false,
+      slap_webhook: 'https://yourwebsite.com/webhook',
+      lap_honey: ''
+      /* These Slapform Name Triggers exist but aren't applicable to AJAX submissions */
+      // slap_redirect: 'https://yourwebsite.com',
+      // slap_captcha: false,
+     },
+     success: function (response) {
+      console.log('Got data: ', response);
+      if (response.meta.status == 'success') {
+       console.log('Submission was successful!');
+       // window.location.href = 'https://google.com' // Uncomment this line if you want to redirect the user upon a successful submission
+      } else if (response.meta.status == 'fail') {
+       console.log('Submission failed with these errors: ', response.meta.errors);
+      }
+    }
+   });
+});
+   
